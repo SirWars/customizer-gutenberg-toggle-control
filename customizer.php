1d2be9eba2c016a7b74a8701cbe1ebf71326ee3d
SirWars <20994642+SirWars@users.noreply.github.com>
2018-11-09 19:37:01 +0000

    Update customizer.php

--- customizer.php
+++ customizer.php
@@ -28,3 +28,17 @@ function text_domain_gutenberg_toggle_register( $wp_customize ) {
 }
 add_action('customize_register', 'text_domain_gutenberg_toggle_register');
 
+
+/**
+ * Checkbox sanitization callback example.
+ *
+ * Sanitization callback for 'checkbox' type controls. This callback sanitizes `$checked`
+ * as a boolean value, either TRUE or FALSE.
+ *
+ * @param bool $checked Whether the checkbox is checked.
+ * @return bool Whether the checkbox is checked.
+ */
+function text_domain_sanitize_checkbox( $checked ) {
+	// Boolean check.
+	return ( ( isset( $checked ) && true === $checked ) ? true : false );
+}
